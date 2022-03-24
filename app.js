885126f3e8b4d039b15d8a902f01a3870ef5d202
julian-anderfuhrn <anderfuhrn5201@gmail.com>
2022-03-23 22:12:11 -0300

    Agrego cambios de heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static("public"));
-app.listen(3050,(req,res)=>{
+app.listen(process.env.PORT || 3050,(req,res)=>{
     console.log("Servidor corriendo");
 })
 app.get("/home",(req,res)=>{
