643d80ed303b75b0af0228e70e34e56afd5134ae
julian-anderfuhrn <anderfuhrn5201@gmail.com>
2022-03-23 22:01:24 -0300

    Agrego al repositorio mi proyecto

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static("public"));
+app.listen(3050,(req,res)=>{
+    console.log("Servidor corriendo");
+})
+app.get("/home",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
