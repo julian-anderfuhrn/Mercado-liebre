7b84c8ca47075d9f53275005edd7f44189ed562b
julian-anderfuhrn <anderfuhrn5201@gmail.com>
2022-03-26 16:16:17 -0300

    Actualizo

--- app.js
+++ app.js
@@ -11,4 +11,7 @@ app.get("/",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/register.html"))
 })
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
 
